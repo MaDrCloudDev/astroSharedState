10e4876a0aee1d9a34e0a50c93ca3ba3a2a0157c
MaDr <97172549+MaDrCloudDev@users.noreply.github.com>
2022-11-22 15:39:40 -0700

    remove tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,8 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import vue from "@astrojs/vue";
 import svelte from "@astrojs/svelte";
-import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), vue(), svelte(), tailwind()]
+  integrations: [react(), vue(), svelte()]
 });
