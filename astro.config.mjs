1c4b8176ba92cb7ada50cb8e01051e6c99fc28d4
MaDr <97172549+MaDrCloudDev@users.noreply.github.com>
2022-11-23 12:22:27 -0700

    fix broken config merge
    fix broken config merge

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,10 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-<<<<<<< HEAD
 import react from "@astrojs/react";
-=======
->>>>>>> 1216a44f811643c70e98da9f7fce286a24dfb175
 import svelte from "@astrojs/svelte";
 import vue from "@astrojs/vue";
 
 // https://astro.build/config
-import react from "@astrojs/react";
-
-// https://astro.build/config
-import vue from "@astrojs/vue";
-
-// https://astro.build/config
 export default defineConfig({
-<<<<<<< HEAD
   integrations: [react(), svelte(), vue()]
-=======
-  integrations: [svelte(), react(), vue()]
->>>>>>> 1216a44f811643c70e98da9f7fce286a24dfb175
 });
