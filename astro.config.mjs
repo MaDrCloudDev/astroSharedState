1216a44f811643c70e98da9f7fce286a24dfb175
MaDr <97172549+MaDrCloudDev@users.noreply.github.com>
2022-11-22 22:16:06 -0700

    clean up
    clean up

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import react from "@astrojs/react";
-import vue from "@astrojs/vue";
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
+import react from "@astrojs/react";
+
+// https://astro.build/config
+import vue from "@astrojs/vue";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [react(), vue(), svelte()]
+  integrations: [svelte(), react(), vue()]
 });
