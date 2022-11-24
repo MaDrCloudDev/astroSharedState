ccd4abf8c6a7e639323f7893998ae76f78f94ef7
MaDr <97172549+MaDrCloudDev@users.noreply.github.com>
2022-11-23 19:08:40 -0700

    add alpinejs for fun
    add alpinejs for fun

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
 import svelte from "@astrojs/svelte";
 import vue from "@astrojs/vue";
 
 // https://astro.build/config
+import alpinejs from "@astrojs/alpinejs";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [react(), svelte(), vue()]
+  integrations: [react(), svelte(), vue(), alpinejs()]
 });
