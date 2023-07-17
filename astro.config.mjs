a9d6fc1a2ea4c7da00be73f2ad11750ed4a3eca1
Matthew Drish <97172549+MaDrCloudDev@users.noreply.github.com>
2023-07-16 19:17:13 -0700

    Astro version update, 3.7.2
    Astro version update, 3.7.2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import alpinejs from "@astrojs/alpinejs";
-import react from "@astrojs/react";
 import solidJs from "@astrojs/solid-js";
+import react from "@astrojs/react";
 import svelte from "@astrojs/svelte";
-
 import vue from "@astrojs/vue";
+
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs(), react(), solidJs(), svelte(), vue()]
+  integrations: [tailwind(), solidJs(), react(), svelte(), vue(), alpinejs()]
 });
